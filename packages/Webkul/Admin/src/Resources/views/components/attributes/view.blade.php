489238e6747a051589bf30b912fdec0825921c50
Thallys Moura <79814738+thallys-moura@users.noreply.github.com>
2024-10-12 12:12:13 -0300

    requisitos 03 - Ajuste DataGrid Vendas; Remoão de Campos do Index de Ordem.

--- packages/Webkul/Admin/src/Resources/views/components/attributes/view.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/attributes/view.blade.php
@@ -5,21 +5,27 @@
     'url'              => null,
 ])
 
+@php
+    $ignoredAttributes = ['lead_source_id', 'lead_type_id'];
+@endphp
 <div class="flex flex-col gap-1">
     @foreach ($customAttributes as $attribute)
-        @if (view()->exists($typeView = 'admin::components.attributes.view.' . $attribute->type))
-            <div class="grid grid-cols-[1fr_2fr] items-center gap-1">
-                <div class="label dark:text-white">{{ $attribute->name }}</div>
 
-                <div class="font-medium dark:text-white">
-                    @include ($typeView, [
-                        'attribute' => $attribute,
-                        'value'     => isset($entity) ? $entity[$attribute->code] : null,
-                        'allowEdit' => $allowEdit,
-                        'url'       => $url,
-                    ])
+        @if (!in_array($attribute->code, $ignoredAttributes))  <!-- Verifica se o código do atributo não está na lista -->
+            @if (view()->exists($typeView = 'admin::components.attributes.view.' . $attribute->type))
+                <div class="grid grid-cols-[1fr_2fr] items-center gap-1">
+                    <div class="label dark:text-white">{{ $attribute->name }}</div>
+
+                    <div class="font-medium dark:text-white">
+                        @include ($typeView, [
+                            'attribute' => $attribute,
+                            'value'     => isset($entity) ? $entity[$attribute->code] : null,
+                            'allowEdit' => $allowEdit,
+                            'url'       => $url,
+                        ])
+                    </div>
                 </div>
-            </div>
+            @endif
         @endif
     @endforeach
 </div>
