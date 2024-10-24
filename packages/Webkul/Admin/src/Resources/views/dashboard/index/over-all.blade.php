0db6e713957657eec906b309b6c59a88f5c7269f
Thallys Moura <79814738+thallys-moura@users.noreply.github.com>
2024-10-24 15:01:33 -0300

    req22_a - remoção da div[total_organization].

--- packages/Webkul/Admin/src/Resources/views/dashboard/index/over-all.blade.php
+++ packages/Webkul/Admin/src/Resources/views/dashboard/index/over-all.blade.php
@@ -129,33 +129,6 @@
                         </div>
                     </div>
                 </div>
-                
-                <!-- Total Organizations -->
-                <div class="flex flex-col gap-2 rounded-lg border border-gray-200 bg-white px-4 py-5 dark:border-gray-800 dark:bg-gray-900">
-                    <p class="text-xs font-medium text-gray-600 dark:text-gray-300">
-                        @lang('admin::app.dashboard.index.over-all.total-organizations')
-                    </p>
-
-                    <div class="flex gap-2">
-                        <p class="text-xl font-bold dark:text-gray-300">
-                            @{{ report.statistics.total_organizations.current }}
-                        </p>
-
-                        <div class="flex items-center gap-0.5">
-                            <span
-                                class="text-base !font-semibold text-green-500"
-                                :class="[report.statistics.total_organizations.progress < 0 ? 'icon-stats-down text-red-500 dark:!text-red-500' : 'icon-stats-up text-green-500 dark:!text-green-500']"
-                            ></span>
-
-                            <p
-                                class="text-xs font-semibold text-green-500"
-                                :class="[report.statistics.total_organizations.progress < 0 ?  'text-red-500' : 'text-green-500']"
-                            >
-                                @{{ Math.abs(report.statistics.total_organizations.progress.toFixed(2)) }}%
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </template>
     </script>
